fb20ce17ea96cd2b4643e24712178638525dcbc9
Kateryna Kuzmina <designkuzmina@gmail.com>
2023-05-29 22:50:09 +0200

    slider not finished yet

--- dist/js/script.js
+++ dist/js/script.js
@@ -93,7 +93,50 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+window.addEventListener('DOMContentLoaded', () => {
+  //slider
+  const prev = document.querySelector('.sliders__arrows_prev'),
+    next = document.querySelector('.sliders__arrows_next'),
+    slides = document.querySelectorAll('.slide'),
+    slidesField = document.querySelector('.sliders__inner'),
+    wrapper = document.querySelector('.sliders__wrapper'),
+    width = window.getComputedStyle(wrapper).width;
+  let offset = 0;
+  const slideWidth = +width.slice(0, width.length - 2); // Ширина одного слайда
+  const maxOffset = slideWidth * (slides.length - 3); // Максимальное смещение
 
+  prev.style.display = 'none';
+  next.addEventListener('click', () => {
+    if (offset === maxOffset) {
+      offset = 0;
+    } else {
+      offset += slideWidth;
+      prev.style.display = 'block';
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+  });
+  prev.addEventListener('click', () => {
+    if (offset === 0) {
+      offset = maxOffset;
+    } else {
+      offset -= slideWidth;
+      next.style.display = 'block';
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+  });
+
+  /* prev.style.display = 'none';
+    next.addEventListener('click', () => {
+      if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+          offset = 0;
+          prev.style.display = 'none';
+      } else {
+          offset += +width.slice(0, width.length - 2);
+          prev.style.display = 'block';
+      }
+      slidesField.style.transform = `translateX(-${offset}px)`;
+  }) */
+});
 
 /***/ })
 
