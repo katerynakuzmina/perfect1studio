f4e031a7affb2e3d42d01fbc0e52ad2592d965ec
Kateryna Kuzmina <designkuzmina@gmail.com>
2023-05-30 12:53:22 +0200

    finished slider, some changes in header & block studio

--- dist/js/script.js
+++ dist/js/script.js
@@ -102,27 +102,25 @@ window.addEventListener('DOMContentLoaded', () => {
     wrapper = document.querySelector('.sliders__wrapper'),
     width = window.getComputedStyle(wrapper).width;
   let offset = 0;
-  const slideWidth = +width.slice(0, width.length - 2); // Ширина одного слайда
+  const slideWidth = +width.slice(0, width.length - 2) / 3; // Ширина одного слайда
   const maxOffset = slideWidth * (slides.length - 3); // Максимальное смещение
 
   prev.style.display = 'none';
   next.addEventListener('click', () => {
-    if (offset === maxOffset) {
-      offset = 0;
-    } else {
-      offset += slideWidth;
-      prev.style.display = 'block';
-    }
+    offset += slideWidth;
+    prev.style.display = 'block';
     slidesField.style.transform = `translateX(-${offset}px)`;
+    if (offset >= maxOffset) {
+      next.style.display = 'none';
+    }
   });
   prev.addEventListener('click', () => {
-    if (offset === 0) {
-      offset = maxOffset;
-    } else {
-      offset -= slideWidth;
-      next.style.display = 'block';
-    }
+    offset -= slideWidth;
+    next.style.display = 'block';
     slidesField.style.transform = `translateX(-${offset}px)`;
+    if (offset <= 0) {
+      prev.style.display = 'none';
+    }
   });
 
   /* prev.style.display = 'none';
